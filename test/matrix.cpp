4dc6156581497b89ce1d35e413e4dd15fc76d3cd
jnyfah <jnyfaah@gmail.com>
2022-07-15 01:44:25 +0100

    tests

--- test/matrix.cpp
+++ test/matrix.cpp
@@ -4,8 +4,6 @@
 #include "matrix.h"
 
 #include "scenepic_tests.h"
-#include "transforms.h"
-
 
 namespace sp = scenepic;
 
@@ -14,10 +12,10 @@ int test_matrix()
     int result = EXIT_SUCCESS;
 
     Eigen::MatrixXf actual(3,4);
+
     actual << 0, -1, 3, -0.03, 0, 2, 0, 3, 0.51, -0.22, 1, 2;
 
-    sp::Color4 vec;
-    vec << -0.87, 1, 0, -1;
+    sp::Color4 vec(-0.87, 1, 0, -1);
 
     Eigen::MatrixXf expected (4,4);
     expected << 0, -1, 3, -0.03, 0, 2, 0, 3, 0.51, -0.22, 1, 2, -0.87, 1, 0, -1;
@@ -30,16 +28,24 @@ int test_matrix()
     top << 0, -1, 3, -0.03, 0, 2, 0, 3;
     bottom <<  0.51, -0.22, 1, 2, -0.87, 1, 0, -1;
 
-
     sp::append_matrix(top, bottom);
     test::assert_allclose(top, expected, result, "append_matrix");
 
-    Eigen::Array<std::uint32_t, Eigen::Dynamic, 1> ExpectedArray(8);
-    ExpectedArray<< 0, 2, 4, 6, 8, 10, 12, 14;
-    test::assert_allclose(sp::arange(0, 17, 2), ExpectedArray, result, "arange");
+    Eigen::Array<std::uint32_t, Eigen::Dynamic, 1> expectedArray(8);
+    expectedArray<< 0, 2, 4, 6, 8, 10, 12, 14;
+    test::assert_allclose(
+        sp::arange(0, 17, 2), expectedArray, result, "arange");
 
+    expected << 0, -1, 3, -0.03, 0, 1, 3, 2.97, 0.51, 0.78, 4, 4.97, -0.36, 1.78, 4, 3.97;
+    test::assert_allclose(
+        sp::rowwise_cumsum(actual), expected, result, "rowwise_cumsum");
 
+    Eigen::Array<std::uint32_t, Eigen::Dynamic, 1> actualArray(8);
 
+    actualArray <<  10, 12, 14, 0, 2, 4, 6, 8;
 
+    test::assert_allclose(
+        sp::roll(actualArray, 5), expectedArray, result, "roll");
+    
     return result;
 }
