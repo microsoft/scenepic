a58ace8b3a57dadf817073bd51d26c0a0d24e3cc
Matthew Johnson (MSRC) <matjoh@microsoft.com>
2022-08-26 16:55:57 +0100

    Bumping the version
    Signed-off-by: Matthew Johnson (MSRC) <matjoh@microsoft.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Microsoft"
 author = "ScenePic Team"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.12"
+release = "1.0.13"
 
 
 # -- General configuration ---------------------------------------------------
